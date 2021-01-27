a38f1f6ea5536918df56c529dcc3e6308a177e0d
bassbuggle <freesound@mmlab.de>
2021-01-27 17:19:26 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-as4elh
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-as4elh)
---
+++ src/app/select-value-binding-example.ts
@@ -0,0 +1,15 @@
+import {Component} from '@angular/core';
+
+/** @title Select with 2-way value binding */
+@Component({
+  selector: 'select-value-binding-example',
+  templateUrl: 'select-value-binding-example.html',
+})
+export class SelectValueBindingExample {
+  selected = 'option2';
+}
+
+
+/**  Copyright 2020 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
